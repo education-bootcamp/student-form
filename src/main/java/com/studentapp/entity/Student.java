305ae7d30b4b67d7e61da702703000817763c537
ozen-hub <info.ozenhub@gmail.com>
2023-09-19 21:10:26 +0530

    7

--- src/main/java/com/studentapp/entity/Student.java
+++ src/main/java/com/studentapp/entity/Student.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Setter
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @ToString
 //@Data -> dont use this with @entity
 @Entity
+@Table
 public class Student {
     @Id
     private long id;
