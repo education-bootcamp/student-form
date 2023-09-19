b9d7b3c426cbea38deda45ea93508fa6d1d6ba0d
ozen-hub <info.ozenhub@gmail.com>
2023-09-19 20:34:10 +0530

    5

--- src/main/java/com/studentapp/controller/HomeController.java
+++ src/main/java/com/studentapp/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.studentapp.controller;
 
+import com.studentapp.dao.StudentDao;
 import com.studentapp.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private StudentDao studentDao;
 
     @RequestMapping(path = "/home")
     public String loadHome(){
@@ -21,7 +26,7 @@ public class HomeController {
 
     @RequestMapping(path = "/create-student", method = RequestMethod.POST)
     public String createStudent(@ModelAttribute Student student){
-        System.out.println(student);
+        studentDao.saveStudent(student);
         return "student-new";
     }
 }
--- src/main/java/com/studentapp/dao/impl/StudentDaoImpl.java
+++ src/main/java/com/studentapp/dao/impl/StudentDaoImpl.java
@@ -2,20 +2,24 @@ package com.studentapp.dao.impl;
 
 import com.studentapp.dao.StudentDao;
 import com.studentapp.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.beans.Customizer;
 import java.util.List;
-
+@Repository
 public class StudentDaoImpl implements StudentDao {
 
+    @Autowired
     private HibernateTemplate hibernateTemplate;
-
+    @Transactional
     public boolean saveStudent(Student student) {
         hibernateTemplate.save(student);
         return true;
     }
-
+    @Transactional
     public boolean updateStudent(Student student) {
         hibernateTemplate.update(student);
         return true;
@@ -24,7 +28,7 @@ public class StudentDaoImpl implements StudentDao {
     public Student getStudent(long id) {
        return hibernateTemplate.get(Student.class,id);
     }
-
+    @Transactional
     public boolean deleteStudent(long id) {
         Student student = hibernateTemplate.get(Student.class, id);
         hibernateTemplate.delete(student);
